23174fc8fa393f838f099a48a14824dc56eb2c2e
amitcodewraps <amit.codewraps@gmail.com>
2022-08-17 00:13:04 +0530

    my first project

--- wp-content/themes/twentytwenty/404.php
+++ wp-content/themes/twentytwenty/404.php
@@ -12,8 +12,13 @@ get_header();
 
 <main id="site-content">
 
-	<div class="section-inner thin error404-content">
+	<!-- <div class="section-inner thin error404-content">
+ -->
 
+ Amit
+
+
+ 
 		<h1 class="entry-title"><?php _e( 'Page Not Found', 'twentytwenty' ); ?></h1>
 
 		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
